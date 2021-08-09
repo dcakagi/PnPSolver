5782a19ce95af58d7df64d6eb57012cb1785b219
dcakagi <dcakagi@gmail.com>
2021-08-09 10:05:38 -0600

    Added opencv pnp solver to PoseEstimator

--- PnPMain.py
+++ PnPMain.py
@@ -15,43 +15,95 @@ if __name__ == '__main__':
     focal_pixels = pixels_width / (2 * np.tan(fov / 2))
 
     estimator = PoseEstimator(constellation, pixels_width, pixels_height, focal_pixels)
+    estimator_cv = PoseEstimator(constellation, pixels_width, pixels_height, focal_pixels, solver_type="opencv")
 
     pixels = {"blue_green": [2247, 1709], "orange": [1677, 1557], "red_magenta": [2040, 1722], "blue": [2395, 1711],
               "green": [1956, 1578], "red": [2224, 1658], "yellow": [2331, 1547], "magenta": [2128, 1504],
               "cyan": [1854, 1568]}
 
-    estimator.updatePose(pixels)
-    rot = estimator.getRotationEstimate()
-    trans = estimator.getTranlsationEstimate()
-    rot = np.array(rot)
-    trans = np.array(trans)
-    print(rot)
-    print(trans)
+    pixels2 = {"blue_green": [2194, 1639],
+               "red_magenta": [2021, 1607],
+               "orange": [1712, 1528],
+               "blue": [2328, 1522],
+               "red": [2185, 1445],
+               "green": [1951, 1439],
+               "cyan": [1861, 1373],
+               "yellow": [2285, 1352],
+               "magenta": [2106, 1318]}
 
-    ell_v_unit_est = rot @ np.array([[1, 0, 0]]).T
-    el_est = np.arctan2(ell_v_unit_est[1, 0], ell_v_unit_est[2, 0])
-    tmp = np.sqrt(ell_v_unit_est[1:2, 0].T @ ell_v_unit_est[1:2, 0])
-    az_est = np.arctan2(ell_v_unit_est[0, 0], tmp)
+    pc_vp_est = estimator.updatePose(pixels)
+    pc_vp_est_cv = estimator_cv.updatePose(pixels)
 
-    #Extract position estimate
-    pc_vp_est = -rot.T @ trans
+    #Extract position estimates
     xc_vp_est = pc_vp_est[0, 0]
     yc_vp_est = pc_vp_est[1, 0]
     zc_vp_est = pc_vp_est[2, 0]
+    xc_vp_est_cv = pc_vp_est_cv[0, 0]
+    yc_vp_est_cv = pc_vp_est_cv[1, 0]
+    zc_vp_est_cv = pc_vp_est_cv[2, 0]
 
     #Actual vehicle position
     x_val = -5870.316222894529
     y_val = -743.7304690000019
-    z_val = -1724.2230763354492
+    z_val = 2219.7767863354493
 
     x_error = abs((xc_vp_est - x_val) / x_val) * 100
     y_error = abs((yc_vp_est - y_val) / y_val) * 100
     z_error = abs((zc_vp_est - z_val) / z_val) * 100
+    x_error_cv = abs((xc_vp_est_cv - x_val) / x_val) * 100
+    y_error_cv = abs((yc_vp_est_cv - y_val) / y_val) * 100
+    z_error_cv = abs((zc_vp_est_cv - z_val) / z_val) * 100
 
-    print(f"X estimate: {xc_vp_est}")
-    print(f"Y estimate: {yc_vp_est}")
-    print(f"Z estimate: {zc_vp_est}")
+    print(f"\nOPnP X estimate: {xc_vp_est}")
+    print(f"OPnP Y estimate: {yc_vp_est}")
+    print(f"OPnP Z estimate: {zc_vp_est}")
 
-    print(f"X % error: {x_error}")
-    print(f"Y % error: {y_error}")
-    print(f"Z % error: {z_error}")
+    print(f"\nOPnP X % error: {x_error}")
+    print(f"OPnP Y % error: {y_error}")
+    print(f"OPnP Z % error: {z_error}")
+
+    print(f"\nOpencv X estimate: {xc_vp_est_cv}")
+    print(f"Opencv Y estimate: {yc_vp_est_cv}")
+    print(f"Opencv Z estimate: {zc_vp_est_cv}")
+
+    print(f"\nOpencv X % error: {x_error_cv}")
+    print(f"Opencv Y % error: {y_error_cv}")
+    print(f"Opencv Z % error: {z_error_cv}")
+
+    pc_vp_est2 = estimator.updatePose(pixels2)
+    pc_vp_est_cv2 = estimator_cv.updatePose(pixels2)
+
+    #Extract position estimate
+    xc_vp_est2 = pc_vp_est2[0, 0]
+    yc_vp_est2 = pc_vp_est2[1, 0]
+    zc_vp_est2 = pc_vp_est2[2, 0]
+    xc_vp_est_cv2 = pc_vp_est_cv2[0, 0]
+    yc_vp_est_cv2 = pc_vp_est_cv2[1, 0]
+    zc_vp_est_cv2 = pc_vp_est_cv2[2, 0]
+
+    x_val2 = -5886.406615960936
+    y_val2 = -743.7304690000019
+    z_val2 = 4184.527266987305
+
+    x_error2 = abs((xc_vp_est2 - x_val2) / x_val2) * 100
+    y_error2 = abs((yc_vp_est2 - y_val2) / y_val2) * 100
+    z_error2 = abs((zc_vp_est2 - z_val2) / z_val2) * 100
+    x_error_cv2 = abs((xc_vp_est_cv2 - x_val2) / x_val2) * 100
+    y_error_cv2 = abs((yc_vp_est_cv2 - y_val2) / y_val2) * 100
+    z_error_cv2 = abs((zc_vp_est_cv2 - z_val2) / z_val2) * 100
+
+    print(f"\nX estimate: {xc_vp_est2}")
+    print(f"Y estimate: {yc_vp_est2}")
+    print(f"Z estimate: {zc_vp_est2}")
+
+    print(f"\nX % error: {x_error2}")
+    print(f"Y % error: {y_error2}")
+    print(f"Z % error: {z_error2}")
+
+    print(f"\nOpencv X estimate: {xc_vp_est_cv}")
+    print(f"Opencv Y estimate: {yc_vp_est_cv}")
+    print(f"Opencv Z estimate: {zc_vp_est_cv}")
+
+    print(f"\nOpencv X % error: {x_error_cv}")
+    print(f"Opencv Y % error: {y_error_cv}")
+    print(f"Opencv Z % error: {z_error_cv}")
--- PoseEstimator.py
+++ PoseEstimator.py
@@ -1,25 +1,30 @@
 import numpy as np
+import cv2
 import matlab.engine
 
 class PoseEstimator:
-    def __init__(self, constellation_: dict, pix_width: int, pix_height: int, focal_pix: float):
+    def __init__(self, constellation_: dict, pix_width: int, pix_height: int, focal_pix: float, solver_type: str="OPnP"):
         '''
         :param constellation_: dictionary of points with unique identifiers as keys and lists of (x, y, z) coordinates as values
         :param pix_width: pixels in image width
         :param pix_height: pixels in image height
-        :param flength_pix: focal length of camera
+        :param focal_pix: focal length of camera
+        :param solver_type: 'OPnP' or 'opencv', type of solver used for estimation
         '''
 
         self.constellation = constellation_
         self.pixel_width = pix_width
         self.pixel_height = pix_height
         self.focal_length_pixels = focal_pix
+        self.solver_type = solver_type
+        self.location_estimate = None
         self.last_rotation = None
         self.last_translation = None
         self.flag = None
 
-        self.eng = matlab.engine.start_matlab()
-        self.eng.addpath('OPnP')
+        if solver_type == "OPnP":
+            self.eng = matlab.engine.start_matlab()
+            self.eng.addpath('OPnP')
 
     def updatePose(self, pixels: dict):
         '''
@@ -34,23 +39,44 @@ class PoseEstimator:
                 pixel_points.append(pixels[id])
                 constellation_points.append(self.constellation[id])
 
-        pixel_points = np.array(pixel_points).T.astype(np.float)
-        constellation_points = np.array(constellation_points).T.astype(np.float)
+        pixel_points = np.array(pixel_points).astype(float)
+        constellation_points = np.array(constellation_points).astype(float)
 
-        pixel_points[0, :] -= 1.0 * self.pixel_width / 2.0
-        pixel_points[1, :] -= 1.0 * self.pixel_height / 2.0
-        pixel_points[1, :] *= -1.0
-        pixel_points = pixel_points / self.focal_length_pixels
+        if self.solver_type == 'OPnP':
+            pixel_points = pixel_points.T
+            constellation_points = constellation_points.T
+            pixel_points[0, :] -= 1.0 * self.pixel_width / 2.0
+            pixel_points[1, :] -= 1.0 * self.pixel_height / 2.0
+            pixel_points[1, :] *= -1.0
+            pixel_points = pixel_points / self.focal_length_pixels
 
-        u = matlab.double(pixel_points.tolist())
-        U = matlab.double(constellation_points.tolist())
-        polish = 'polish'
+            u = matlab.double(pixel_points.tolist())
+            U = matlab.double(constellation_points.tolist())
+            polish = 'polish'
 
-        [R0, t0, error0, flag] = self.eng.OPnP(U, u, polish, nargout=4)
+            [R0, t0, error0, flag] = self.eng.OPnP(U, u, polish, nargout=4)
 
-        self.last_rotation = R0
-        self.last_translation = t0
-        self.flag = flag
+            self.last_rotation = np.array(R0)
+            self.last_translation = np.array(t0)
+            self.flag = flag
+        elif self.solver_type == 'opencv':
+            constellation_points = np.expand_dims(constellation_points, axis=2)
+            pixel_points = np.expand_dims(pixel_points, axis=2)
+            cam_mat = np.array([[self.focal_length_pixels, 0, self.pixel_width / 2],
+                                [0, self.focal_length_pixels, self.pixel_height / 2],
+                                [0, 0, 1]])
+            distortion = None
+
+            ret, rvecs, tvecs = cv2.solvePnP(constellation_points, pixel_points, cam_mat, distortion)
+            self.last_rotation = cv2.Rodrigues(rvecs)[0]
+            self.last_translation = tvecs ## Check for array type
+
+        self.location_estimate = -self.last_rotation.T @ self.last_translation
+
+        return self.location_estimate
+
+    def getLocationEstimate(self):
+        return self.location_estimate
 
     def getRotationEstimate(self):
         return self.last_rotation
