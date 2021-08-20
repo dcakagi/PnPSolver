a8c383bb2ab01a13ae0a635d22c45b27f234888d
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-08-20 15:52:00 -0600

    Auto whitespace removal

--- simulation/read_data.py
+++ simulation/read_data.py
@@ -13,6 +13,6 @@ for i in np.arange(343):
     #grab the pixel locations
     pixel_location_file = open("data_300m/pixel_locations/px_loc_" + str(i) + ".pkl", "rb")
     px_locations = pickle.load(pixel_location_file)
-    
-    # to get the corresponding image 
+
+    # to get the corresponding image
     # image = cv2.imread("data_300m/pictures/pic_" + str(i))
